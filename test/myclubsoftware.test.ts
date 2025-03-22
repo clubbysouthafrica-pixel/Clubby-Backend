d7f5d2bd566794463adad03993cd2bdb55a72b8a
gregtorrington <gregt@aleios.com>
2025-03-22 14:39:01 +0200

    Initial commit

---
+++ test/myclubsoftware.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Myclubsoftware from '../lib/myclubsoftware-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/myclubsoftware-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Myclubsoftware.MyclubsoftwareStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
