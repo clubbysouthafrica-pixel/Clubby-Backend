ce37db2c6ec78c4705bc69d51eb43a415d3be4f1
gregtorrington <gregt@aleios.com>
2025-06-06 23:17:52 +0200

    feat: improving backend

--- functions/member/registration/submit_registration/submit_registration.ts
+++ functions/member/registration/submit_registration/submit_registration.ts
@@ -1,11 +1,5 @@
-import {
-    DynamoDBClient,
-    PutItemCommand,
-} from "@aws-sdk/client-dynamodb";
 import { unmarshall } from "@aws-sdk/util-dynamodb";
 import { createResponse, deconstructEvent, addItem, queryItems, getItem } from "./function_helpers";
-
-const dynamodbClient = new DynamoDBClient({ region: process.env.REGION });
 
 export type InputType = 'TEXT' | 'DROPDOWN' | 'PHONE' | 'DATE';
 export type CurrencyType = 'DOLLAR' | 'RAND' | 'EURO' | 'POUND' | 'NEW ZEALAND DOLLAR' | 'AUSTRALIAN DOLLAR';
