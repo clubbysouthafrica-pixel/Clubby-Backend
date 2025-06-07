1971e13242941e1562287dec9fc9962f884010fc
gregtorrington <gregt@aleios.com>
2025-06-07 19:34:28 +0200

    feat: improving backend

--- functions/login/sign_in/function_helpers/database_functions/index.ts
+++ functions/login/sign_in/function_helpers/database_functions/index.ts
@@ -2,3 +2,4 @@ export { getItem } from "./get_item";
 export { queryItems } from "./query_items";
 export { addItem } from "./add_item";
 export { updateItem } from "./update_item";
+export { scanItems } from "./scan_items";
