026079d350649dd12ec98db3790ab0987dd62893
gregtorrington <gregt@aleios.com>
2025-06-07 19:06:54 +0200

    feat: improving backend

--- functions/member/club/get_all_clubs/function_helpers/index.ts
+++ functions/member/club/get_all_clubs/function_helpers/index.ts
@@ -1,4 +1,4 @@
 export { createResponse } from "./function-responses";
 export { ACCESS, CLUB_TYPES } from "./constants";
 export { deconstructEvent } from "./deconstruct_event";
-export { getItem, queryItems, addItem } from "./database_functions";
+export { getItem, queryItems, addItem, updateItem } from "./database_functions";
--- functions/member/club/get_club/function_helpers/database_functions/index.ts
+++ functions/member/club/get_club/function_helpers/database_functions/index.ts
@@ -1,3 +1,4 @@
 export { getItem } from "./get_item";
 export { queryItems } from "./query_items";
 export { addItem } from "./add_item";
+export { updateItem } from "./update_item";
