ea1d8cb3b2dc96fd30fe24fb357f7c81890d3f1c
gregtorrington <gregt@aleios.com>
2025-06-10 21:16:23 +0200

    feat: improving backend

--- functions/login/sign_up/function_helpers/deconstruct_event.ts
+++ functions/login/sign_up/function_helpers/deconstruct_event.ts
@@ -1,8 +1,13 @@
+import jwt from "jsonwebtoken"
+
 export const deconstructEvent = (event: any) => {
     console.log(`EVENT @ ${new Date()}: `, event);
     
     const origin = event.headers.origin;
     console.log(`Called by origin: ${origin}`)
+
+    const decoded = jwt.decode(event.headers.Authorization);
+    console.log('Decoded: ', decoded)
     
     const body = JSON.parse(event.body);
     const query_string_params = event.queryStringParameters;
