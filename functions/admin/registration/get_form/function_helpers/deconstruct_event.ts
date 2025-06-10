28e99f6840f92b66b71ffb5f44d74ad71b96db30
gregtorrington <gregt@aleios.com>
2025-06-10 21:37:12 +0200

    feat: improving backend

--- functions/admin/registration/get_form/function_helpers/deconstruct_event.ts
+++ functions/admin/registration/get_form/function_helpers/deconstruct_event.ts
@@ -1,16 +1,20 @@
 import jwt from "jsonwebtoken"
 
-export const deconstructEvent = (event: any) => {
+export const deconstructEvent = (event: any, get_user_id=true) => {
     console.log(`EVENT @ ${new Date()}: `, event);
     
     const origin = event.headers.origin;
     console.log(`Called by origin: ${origin}`)
 
-    const decoded = jwt.decode(event.headers.Authorization);
-    const user_id = decoded?.sub as string;
-    
     const body = JSON.parse(event.body);
     const query_string_params = event.queryStringParameters;
 
-    return { origin, body, query_string_params, user_id }
+    if (get_user_id) {
+        const decoded = jwt.decode(event.headers?.Authorization);
+        const user_id = decoded?.sub as string;
+
+        return { origin, body, query_string_params, user_id }
+    }
+
+    return { origin, body, query_string_params }
 };
