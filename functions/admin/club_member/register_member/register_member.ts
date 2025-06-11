d0b00f5b8943f2145d58add8b1fbee2e7c840f7a
gregtorrington <gregt@aleios.com>
2025-06-11 22:49:39 +0200

    feat: improving backend

--- functions/admin/club_member/register_member/register_member.ts
+++ functions/admin/club_member/register_member/register_member.ts
@@ -6,17 +6,17 @@ export const handler = async (event: any) => {
 
     try {
 
-        if (query_string_params?.club_account_id == null || body?.member_billing_type == null) {
-            return createResponse(400, { message: "Invalid request. club_account_id required in query string params. member_billing_type requried in body." }, origin);
+        if (body?.club_account_id == null || body?.member_billing_type == null || body.member_id == null) {
+            return createResponse(400, { message: "Invalid request. club_account_id, member_billing_type, member_id requried in body." }, origin);
         }
-        if (typeof query_string_params.club_account_id !== 'string' || typeof body.member_billing_type !== 'string') {
-            return createResponse(400, { message: "club_account_id and member_billing_type must be STRING type." }, origin);
+        if (typeof body.club_account_id !== 'string' || typeof body.member_billing_type !== 'string' || typeof body.member_id !== 'string') {
+            return createResponse(400, { message: "club_account_id, member_billing_type, member_id must be STRING type." }, origin);
         }
 
         const registration_billing = await getItem(
             process.env.REGISTRATION_FORM_TABLE_NAME as string,
             {
-                club_account_id: query_string_params.club_account_id,
+                club_account_id: body.club_account_id,
                 field_name: body.member_billing_type
             }
         );
@@ -28,8 +28,8 @@ export const handler = async (event: any) => {
         await updateItem(
             process.env.CLUB_MEMBER_TABLE_NAME as string,
             {
-              user_id: user_id as string,
-              club_account_id: query_string_params.club_account_id,
+              user_id: body.member_id,
+              club_account_id: body.club_account_id,
             },
             "SET #reg = :registered, #amount = #amount - :deduct_amount",
             {
