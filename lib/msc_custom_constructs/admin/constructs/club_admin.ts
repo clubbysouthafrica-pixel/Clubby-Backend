b163b2234c8180327c62cbba28d70759e313585e
gregtorrington <gregt@aleios.com>
2025-06-11 08:53:54 +0200

    feat: endpoint for getting all admin clubs

--- lib/msc_custom_constructs/admin/constructs/club_admin.ts
+++ lib/msc_custom_constructs/admin/constructs/club_admin.ts
@@ -1,7 +1,7 @@
 import { Construct } from "constructs";
 import { MSC_Lambda, MSC_APIGateway, MSC_Table } from "../../../msc_service_constructs";
 import { addCorsEnabledMethod } from "../../../msc_custom_functions";
-import { TokenAuthorizer } from "aws-cdk-lib/aws-apigateway";
+import { AuthorizationType, MethodOptions, TokenAuthorizer } from "aws-cdk-lib/aws-apigateway";
 import { MSC_Layers } from "../../lambda_layers";
 
 interface MSC_ClubAdminClubConstructProps {
@@ -39,16 +39,32 @@ export class MSC_ClubAdminClubConstruct extends Construct {
             layers: [props.layers.jwt_layer]
         });
 
+        const get_all_admin_clubs = new MSC_Lambda(this, `${id}-GetAllAdminClubs`, {
+            code: "admin/club_admin/get_all_admin_clubs",
+            envVariables: {
+                CLUB_ADMIN_TABLE_NAME: props.club_admin_table.tableName,
+                ADMIN_TOKEN: "FHJ289489JDJD"
+            },
+            permissions: {
+                [props.club_admin_table.tableArn]: [
+                    "dynamodb:Query"
+                ]
+            },
+            layers: [props.layers.jwt_layer]
+        });
+
         const admin_club_resource = props.api_gateway.root.addResource("clubAdmin");
 
         const associate_user_with_club_resource = admin_club_resource.addResource("associateUserWithClub");
+        const get_all_admin_clubs_resource = admin_club_resource.addResource("getAllAdminClubs");
 
-        // const methodOptions: MethodOptions = {
-        //     methodResponses: [],
-        //     authorizationType: AuthorizationType.CUSTOM,
-        //     authorizer: props.token_authorizer
-        // }
+        const methodOptions: MethodOptions = {
+            methodResponses: [],
+            authorizationType: AuthorizationType.CUSTOM,
+            authorizer: props.token_authorizer
+        }
 
         addCorsEnabledMethod(associate_user_with_club_resource, associate_user_with_club, { methodResponses: [] }, undefined, "PUT");
+        addCorsEnabledMethod(get_all_admin_clubs_resource, get_all_admin_clubs, methodOptions, undefined, "GET");
     }
 }
