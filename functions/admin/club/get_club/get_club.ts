b9569d5eae09dfd793d992f94863381b3956defd
gregtorrington <gregt@aleios.com>
2025-06-23 20:56:15 +0200

    feat: improving backend

---
+++ functions/admin/club/get_club/get_club.ts
@@ -0,0 +1,39 @@
+import { createResponse, deconstructEvent, getItem } from "./function_helpers";
+
+export const handler = async (event: any) => {
+
+    const { origin, body, query_string_params, user_id } = deconstructEvent(event);
+
+    try {
+
+        if (query_string_params?.club_account_id == null) {
+            return createResponse(400, { message: "club_type and club_account_id required." }, origin);
+        }
+
+        const item = await getItem(process.env.CLUB_TABLE_NAME as string, {
+            club_account_id: query_string_params.club_account_id
+        });
+
+        if (item == null) {
+            return createResponse(400, { message: "Club not found." }, origin);
+        }
+
+        const club_member = await getItem(process.env.CLUB_MEMBER_TABLE_NAME as string, {
+            club_account_id: query_string_params.club_account_id,
+            user_id: user_id as string
+        });
+
+        const member_exists = club_member ? true : false;
+
+        return createResponse(200, {
+            club_account_id: item["club_account_id"],
+            club_type: item["club_type"],
+            club_name: item["club_name"],
+            club_member_exists: member_exists
+        }, origin);
+
+    } catch (error) {
+        console.error("Error:", error);
+        return createResponse(500, { message: "Internal Server Error" }, origin);
+    }
+};
--- lib/msc_custom_constructs/admin/constructs/club.ts
+++ lib/msc_custom_constructs/admin/constructs/club.ts
@@ -1,7 +1,7 @@
 import { Construct } from "constructs";
 import { MSC_Lambda, MSC_APIGateway, MSC_Table } from "../../../msc_service_constructs";
 import { addCorsEnabledMethod } from "../../../msc_custom_functions";
-import { TokenAuthorizer } from "aws-cdk-lib/aws-apigateway";
+import { AuthorizationType, MethodOptions, TokenAuthorizer } from "aws-cdk-lib/aws-apigateway";
 import { MSC_Layers } from "../../lambda_layers";
 
 interface MSC_AdminClubConstructProps {
@@ -29,16 +29,31 @@ export class MSC_AdminClubConstruct extends Construct {
             layers: [props.layers.jwt_layer]
         });
 
+        const get_club = new MSC_Lambda(this, `${id}-GetClub`, {
+            code: "admin/club/get_club",
+            envVariables: {
+                CLUB_TABLE_NAME: props.club_table.tableName
+            },
+            permissions: {
+                [props.club_table.tableArn]: [
+                    "dynamodb:GetItem"
+                ]
+            },
+            layers: [props.layers.jwt_layer]
+        });
+
         const club_resource = props.api_gateway.root.addResource("club");
 
         const create_club_resource = club_resource.addResource("createClub");
+        const get_club_resource = club_resource.addResource("getClub");
 
-        // const methodOptions: MethodOptions = {
-        //     methodResponses: [],
-        //     authorizationType: AuthorizationType.CUSTOM,
-        //     authorizer: props.token_authorizer
-        // }
+        const methodOptions: MethodOptions = {
+            methodResponses: [],
+            authorizationType: AuthorizationType.CUSTOM,
+            authorizer: props.token_authorizer
+        }
 
         addCorsEnabledMethod(create_club_resource, create_club, { methodResponses: [] }, undefined, "PUT");
+        addCorsEnabledMethod(get_club_resource, get_club, methodOptions, undefined, "GET");
     }
 }
