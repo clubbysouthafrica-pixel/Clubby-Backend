ccb2a131f9311c64fe5716cb25165b84483c08de
gregtorrington <gregt@aleios.com>
2025-06-26 14:56:14 +0200

    feat: improving backend

--- lib/msc_custom_constructs/mailer/constructs/send_mail.ts
+++ lib/msc_custom_constructs/mailer/constructs/send_mail.ts
@@ -2,7 +2,7 @@ import { Construct } from "constructs";
 import { MSC_Lambda, MSC_Queue } from "../../../msc_service_constructs";
 import { SqsEventSource } from "aws-cdk-lib/aws-lambda-event-sources";
 
-interface MSC_SendEmailConstructProps {}
+interface MSC_SendEmailConstructProps { }
 
 export class MSC_SendEmailConstruct extends Construct {
     public readonly mail_queue: MSC_Queue;
@@ -11,7 +11,7 @@ export class MSC_SendEmailConstruct extends Construct {
 
         this.mail_queue = new MSC_Queue(this, `${id}-Mail`, {
             queue_name: 'SendMail'
-          });
+        });
 
         const send_mail = new MSC_Lambda(this, id, {
             code: "mailer/send_mail",
