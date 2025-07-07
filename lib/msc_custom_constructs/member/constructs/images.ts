6d324fc994b1b888a636919b934f8b2e10931198
gregtorrington <gregt@aleios.com>
2025-07-07 21:10:01 +0200

    feat: improving backend

--- lib/msc_custom_constructs/member/constructs/images.ts
+++ lib/msc_custom_constructs/member/constructs/images.ts
@@ -16,7 +16,7 @@ export class MSC_ImagesConstruct extends Construct {
     constructor(scope: Construct, id: string, props: MSC_ImagesConstructProps) {
         super(scope, id);
 
-        const generate_cover_presigned_url = new MSC_Lambda(this, `${id}-CoverPresignedURL`, {
+        const generate_club_cover_presigned_url = new MSC_Lambda(this, `${id}-ClubCoverPresignedURL`, {
             code: "member/images/generate_cover_presigned_url",
             envVariables: {
                 IMAGE_BUCKET_NAME: props.image_bucket.bucketName,
@@ -29,7 +29,7 @@ export class MSC_ImagesConstruct extends Construct {
             },
             layers: [props.layers.jwt_layer]
         });
-        props.image_bucket.grantRead(generate_cover_presigned_url);
+        props.image_bucket.grantRead(generate_club_cover_presigned_url);
 
         const generate_profile_presigned_url = new MSC_Lambda(this, `${id}-ProfilePresignedURL`, {
             code: "member/images/generate_profile_presigned_url",
@@ -43,7 +43,7 @@ export class MSC_ImagesConstruct extends Construct {
 
         const images_resource = props.api_gateway.root.addResource("images");
 
-        const generate_cover_presigned_url_resource = images_resource.addResource("presignedCoverUrl");
+        const generate_club_cover_presigned_url_resource = images_resource.addResource("presignedClubCoverUrl");
         const generate_profile_presigned_url_resource = images_resource.addResource("presignedProfileUrl");
 
         const methodOptions: MethodOptions = {
@@ -52,7 +52,7 @@ export class MSC_ImagesConstruct extends Construct {
             authorizer: props.token_authorizer
         }
 
-        addCorsEnabledMethod(generate_cover_presigned_url_resource, generate_cover_presigned_url, methodOptions, undefined, "GET");
+        addCorsEnabledMethod(generate_club_cover_presigned_url_resource, generate_club_cover_presigned_url, methodOptions, undefined, "GET");
         addCorsEnabledMethod(generate_profile_presigned_url_resource, generate_profile_presigned_url, methodOptions, undefined, "GET");
     }
 }
