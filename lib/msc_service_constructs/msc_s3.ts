a9ba0d1769a6d5576d77b7760462141d6227b1f9
gregtorrington <gregt@aleios.com>
2025-07-10 20:53:22 +0200

    feat: improving backend

--- lib/msc_service_constructs/msc_s3.ts
+++ lib/msc_service_constructs/msc_s3.ts
@@ -2,30 +2,29 @@ import { RemovalPolicy } from "aws-cdk-lib";
 import { Bucket, BucketEncryption, HttpMethods } from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 
-
 interface MSC_BucketProps {
-    bucket_name: string;
-    enableCors?: boolean;
+  bucket_name: string;
+  enableCors?: boolean;
 }
 
 export class MSC_Bucket extends Bucket {
-    constructor(scope: Construct, id: string, props: MSC_BucketProps) {
-        super(scope, `${id}-Bucket`, {
-            bucketName: props.bucket_name,
-            encryption: BucketEncryption.S3_MANAGED,
-            removalPolicy: RemovalPolicy.DESTROY,
-            cors: props.enableCors ? [
-                {
-                  allowedMethods: [
-                    HttpMethods.GET,
-                    HttpMethods.PUT
-                  ],
-                  allowedOrigins: ["http://localhost:5173"],
-                  allowedHeaders: ["*"],
-                  maxAge: 3600
-                },
-              ] : undefined,
-            autoDeleteObjects: true
-        })
-    }
+  constructor(scope: Construct, id: string, props: MSC_BucketProps) {
+    super(scope, `${id}-Bucket`, {
+      bucketName: props.bucket_name,
+      encryption: BucketEncryption.S3_MANAGED,
+      removalPolicy: RemovalPolicy.DESTROY,
+      cors: props.enableCors ? [
+        {
+          allowedMethods: [
+            HttpMethods.GET,
+            HttpMethods.PUT
+          ],
+          allowedOrigins: ["http://localhost:5173"],
+          allowedHeaders: ["*"],
+          maxAge: 3600
+        },
+      ] : undefined,
+      autoDeleteObjects: true
+    })
+  }
 }
