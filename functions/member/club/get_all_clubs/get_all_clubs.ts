6cc46ddf4bee3e2cd3fa5e53c29bbc57c6092fbe
gregtorrington <gregt@aleios.com>
2025-07-21 20:27:50 +0200

    feat: improving backend

--- functions/member/club/get_all_clubs/get_all_clubs.ts
+++ functions/member/club/get_all_clubs/get_all_clubs.ts
@@ -1,4 +1,8 @@
+import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { createResponse, deconstructEvent, scanItems } from "./function_helpers";
+
+const s3_client = new S3Client({ region: process.env.REGION });
 
 export const handler = async (event: any) => {
 
@@ -8,9 +12,25 @@ export const handler = async (event: any) => {
         const clubs = await scanItems(process.env.CLUB_TABLE_NAME as string)
 
         let items: any[] = [];
-        items = clubs.map((item) => {
+        items = clubs.map(async (item) => {
+            const cover_key = `club_cover/${item.club_account_id}_cover`;
+            const getCoverCommand = new GetObjectCommand({
+                Bucket: process.env.IMAGE_BUCKET_NAME,
+                Key: cover_key,
+            });
+            const get_cover_url = await getSignedUrl(s3_client, getCoverCommand, { expiresIn: 60 * 5 });
+
+            const profile_key = `club_cover/${item.club_account_id}_profile`;
+            const getProfileCommand = new GetObjectCommand({
+                Bucket: process.env.IMAGE_BUCKET_NAME,
+                Key: profile_key,
+            });
+            const get_profile_url = await getSignedUrl(s3_client, getProfileCommand, { expiresIn: 60 * 5 });
+
             return {
                 club_name: item.club_name,
+                club_cover_url: get_cover_url,
+                club_profile_url: get_profile_url,
                 club_account_id: item.club_account_id,
                 club_type: item.club_type
             };
--- lib/msc_custom_constructs/member/member_stack.ts
+++ lib/msc_custom_constructs/member/member_stack.ts
@@ -72,6 +72,7 @@ export class MSC_MemberNestedStack extends Stack {
             club_table: props.club_table,
             club_member_table: props.club_member_table,
             token_authorizer: jwt_construct.token_authorizer,
+            image_bucket: props.image_bucket,
             layers: props.layers
         });
 
