3bd35003eb6b2b88161ecafa8be9fc44896711bf
gregtorrington <gregt@aleios.com>
2025-07-21 20:52:27 +0200

    feat: improving backend

--- functions/member/club/get_all_clubs/get_all_clubs.ts
+++ functions/member/club/get_all_clubs/get_all_clubs.ts
@@ -11,22 +11,21 @@ export const handler = async (event: any) => {
     try {
         const clubs = await scanItems(process.env.CLUB_TABLE_NAME as string)
 
-        let items: any[] = [];
-        items = clubs.map(async (item) => {
+        const items = await Promise.all(clubs.map(async (item) => {
             const cover_key = `club_cover/${item.club_account_id}_cover`;
             const getCoverCommand = new GetObjectCommand({
                 Bucket: process.env.IMAGE_BUCKET_NAME,
                 Key: cover_key,
             });
             const get_cover_url = await getSignedUrl(s3_client, getCoverCommand, { expiresIn: 60 * 5 });
-
+        
             const profile_key = `club_cover/${item.club_account_id}_profile`;
             const getProfileCommand = new GetObjectCommand({
                 Bucket: process.env.IMAGE_BUCKET_NAME,
                 Key: profile_key,
             });
             const get_profile_url = await getSignedUrl(s3_client, getProfileCommand, { expiresIn: 60 * 5 });
-
+        
             return {
                 club_name: item.club_name,
                 club_cover_url: get_cover_url,
@@ -34,7 +33,7 @@ export const handler = async (event: any) => {
                 club_account_id: item.club_account_id,
                 club_type: item.club_type
             };
-        });
+        }));
 
         return createResponse(200, { items }, origin);
 
