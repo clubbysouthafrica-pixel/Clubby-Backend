354bb2c5bee69ef1170cb69eda174d7b50f04392
gregtorrington <gregt@aleios.com>
2025-08-05 08:57:53 +0200

    feat improving backend

--- functions/internal_infra/club/create_club/create_club.ts
+++ functions/internal_infra/club/create_club/create_club.ts
@@ -14,16 +14,29 @@ export const handler = async (event: any) => {
             return createResponse(400, { message: 'Not authorized for admin signup.' }, origin);
         }
 
-        if (body?.club_type == null || body?.club_name == null || body?.member_registration_fee == null || body?.verified_identity == null) {
-            return createResponse(400, { message: 'club_type, member_registration_fee, verified_identity and club_name required.' }, origin);
+        if (
+            body?.club_type == null || 
+            body?.club_name == null || 
+            body?.member_registration_fee_to_club == null || 
+            body?.verified_identity == null ||
+            body?.maximum_monthly_emails == null || 
+            body?.fee_per_email_to_club == null ||
+            body?.free_email_limit == null
+        ) {
+            return createResponse(400, { message: 'club_type, member_registration_fee_to_club, verified_identity, maximum_monthly_emails, fee_per_email_to_club, free_email_limit and club_name required.' }, origin);
         }
 
         if (!CLUB_TYPES.includes(body.club_type)) {
             return createResponse(400, { message: `Invalid club_type. Valid values: ${CLUB_TYPES}.` }, origin);
         }
 
-        if (typeof body.member_registration_fee !== 'number') {
-            return createResponse(400, { message: "member_registration_fee must be of type number." }, origin)
+        if (
+            typeof body.member_registration_fee_to_club !== 'number' || 
+            typeof body.maximum_monthly_emails !== 'number' || 
+            typeof body.fee_per_email_to_club !== 'number' ||
+            typeof body.free_email_limit !== 'number'
+        ) {
+            return createResponse(400, { message: "member_registration_fee_to_club, maximum_monthly_emails, fee_per_email_to_club, free_email_limit must be of type number." }, origin)
         }
 
         const club_account_id = generate_club_Id(body.club_name);
@@ -35,7 +48,10 @@ export const handler = async (event: any) => {
                 "verified_identity": body.verified_identity,
                 "club_name": body.club_name,
                 "club_account_id": club_account_id,
-                "member_registration_fee": body.member_registration_fee
+                "member_registration_fee_to_club": body.member_registration_fee_to_club,
+                "maximum_monthly_emails": body.maximum_monthly_emails,
+                "fee_per_email_to_club": body.fee_per_email_to_club,
+                "free_email_limit": body.free_email_limit
             }
         );
 
