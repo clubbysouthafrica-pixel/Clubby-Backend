cae1900f878c5bbb7b8bdfad878a4219e8809942
gregtorrington <gregt@aleios.com>
2025-09-07 17:46:48 +0200

    feat: improving backend

--- functions/admin/deregistration/deregister_season/deregister_season.ts
+++ functions/admin/deregistration/deregister_season/deregister_season.ts
@@ -55,15 +55,18 @@ export const handler = async (event: any) => {
                 },
                 `SET 
                     #registered = :registered,
-                    #outstanding_amount = #registration_amount
+                    #outstanding_amount = #registration_amount,
+                    #resubmission_required = :resubmission_required
                 `,
                 {
                     "#registered": "registered",
                     "#outstanding_amount": "outstanding_amount",
                     "#registration_amount": "registration_amount",
+                    "#resubmission_required": "resubmission_required"
                 },
                 {
-                    ":registered": false
+                    ":registered": false,
+                    ":resubmission_required": true
                 }
             );
         }
