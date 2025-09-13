a84ab23ca1ae2ed6432e37b9a2c756d5aedbdec7
gregtorrington <gregt@aleios.com>
2025-09-13 11:49:58 +0200

    fix: registration form

--- functions/admin/registration/create_registration_form/create_registration_form.ts
+++ functions/admin/registration/create_registration_form/create_registration_form.ts
@@ -57,7 +57,7 @@ function isBillingField(obj: any): obj is BillingField {
 }
 
 function isStandardField(obj: any): obj is StandardField {
-    const validTypes = ['TEXT', 'DROPDOWN', 'PHONE', 'DATE', 'NUMBER', 'RADIO'];
+    const validTypes = ['TEXT', 'DROPDOWN', 'PHONE', 'DATE', 'NUMBER', 'CHECKBOX'];
 
     return obj.field_type === 'STANDARD' &&
         validTypes.includes(obj.input_type) &&
