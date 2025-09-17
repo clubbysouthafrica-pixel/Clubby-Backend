d026f4f4493fad1c6439a69a94cd8df5d4f3a504
gregtorrington <gregt@aleios.com>
2025-09-17 19:17:35 +0200

    feat: improving backend

--- functions/admin/club/get_club/get_club.ts
+++ functions/admin/club/get_club/get_club.ts
@@ -22,7 +22,9 @@ export const handler = async (event: any) => {
             club_account_id: item["club_account_id"],
             club_type: item["club_type"],
             club_name: item["club_name"],
-            description: item["description"] ?? undefined
+            description: item["description"] ?? undefined,
+            support_email: item["support_email"],
+            country_of_operation: item["country_of_operation"]
         }, origin);
 
     } catch (error) {
