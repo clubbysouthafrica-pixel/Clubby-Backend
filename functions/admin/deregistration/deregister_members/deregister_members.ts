277388281cdb9467af1ef00e3cb98c7ab75f84a7
gregtorrington <gregt@aleios.com>
2025-09-25 17:54:24 +0200

    feat: improving backend

--- functions/admin/deregistration/deregister_members/deregister_members.ts
+++ functions/admin/deregistration/deregister_members/deregister_members.ts
@@ -54,13 +54,10 @@ async function updateRegistrationsTable(user_id: string, registration_id: string
             "user_id": user_id
         },
         `SET 
-            #deregistered = :deregistered,
-            #total_outstanding_amount = #total_fee
+            #deregistered = :deregistered
         `,
         {
-            "#deregistered": "deregistered",
-            "#total_outstanding_amount": "total_outstanding_amount",
-            "#total_fee": "total_fee"
+            "#deregistered": "deregistered"
         },
         {
             ":deregistered": true
