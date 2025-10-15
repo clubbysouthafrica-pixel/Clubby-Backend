30cc10c4b97c33fa076d0acc2e16985ecefc93d5
gregtorrington <gregt@aleios.com>
2025-10-15 14:39:24 +0200

    fix: improving backend

--- functions/admin/club/get_club/get_club.ts
+++ functions/admin/club/get_club/get_club.ts
@@ -21,6 +21,7 @@ export const handler = async (event: any) => {
         return createResponse(200, {
             club_account_id: item["club_account_id"],
             club_type: item["club_type"],
+            season_cycle: item?.season_cycle ?? 1,
             club_name: item["club_name"],
             description: item["description"] ?? undefined,
             address: item["address"] ?? undefined,
