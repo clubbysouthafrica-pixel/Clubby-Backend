a677c03e45961f9c7ecfabbb7e233f9ce2d0962e
gregtorrington <gregt@aleios.com>
2025-10-19 16:53:40 +0200

    feat: improving backend

--- functions/admin/club_member/get_all_club_members/function_helpers/database_functions/add_item.ts
+++ functions/admin/club_member/get_all_club_members/function_helpers/database_functions/add_item.ts
@@ -22,7 +22,7 @@ export const addItem = async (
         console.log(`@@@ addItem request (Table_Name: ${table_name}): `, JSON.stringify(request));
         const command = new PutItemCommand({
             TableName: table_name,
-            Item: marshall(item),
+            Item: marshall(item, { removeUndefinedValues: true }),
             ConditionExpression: condition_expression ?? undefined,
         });
         const response = await dynamodbClient.send(command);
