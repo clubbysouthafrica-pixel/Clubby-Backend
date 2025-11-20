39b8378e46b98a7aaf7cd061ba9c889bee29f78a
gregtorrington <gregt@aleios.com>
2025-11-20 08:51:16 +0200

    feat: improving backend

--- functions/admin/club_member/get_all_club_members/get_all_club_members.ts
+++ functions/admin/club_member/get_all_club_members/get_all_club_members.ts
@@ -90,7 +90,8 @@ export const handler = async (event: any) => {
         )
 
         const filters: any[] = []
-        form?.forEach(field => {
+        for (const field of form || []) {
+            if (field?.visible !== true) continue
             if (field.field_type === "BILLING" && field.input_type === "DROPDOWN") {
                 filters.push(
                     {
@@ -127,8 +128,16 @@ export const handler = async (event: any) => {
                         type: "standard"
                     }
                 )
+            } else if (field.field_type === "STANDARD" && field.input_type === "TEXT") {
+                filters.push(
+                    {
+                        key: `standard:${field.field_name}`,
+                        field_name: field.field_name,
+                        type: "standard"
+                    }
+                )
             }
-        })
+        }
 
         return createResponse(200, { registered, unregistered, filters }, origin);
 
