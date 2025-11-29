32c88e85a8c40785f460a541c90471d95fb88346
gregtorrington <gregt@aleios.com>
2025-11-29 09:27:10 +0200

    feat: improving backend

--- functions/member/payfast/handle_payment/handle_payment.ts
+++ functions/member/payfast/handle_payment/handle_payment.ts
@@ -284,6 +284,6 @@ export const handler = async (event: any) => {
         return { statusCode: 200, body: "OK" };
     } else {
         console.error("❌ Payment verification failed");
-        return { statusCode: 200, body: "OK" };
+        return { statusCode: 400, body: "Payment failed" };
     }
 };
