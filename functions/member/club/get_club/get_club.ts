5759448d98e52b760d7f1863aeb5a521f8c42489
gregtorrington <gregt@aleios.com>
2025-12-16 08:26:04 +0200

    feat: improving backend

--- functions/member/club/get_club/get_club.ts
+++ functions/member/club/get_club/get_club.ts
@@ -103,6 +103,7 @@ export const handler = async (event: any) => {
             country_of_operation: item["country_of_operation"],
             joined: item["joined"],
             payfast_enabled: item?.payfast_enabled ?? false,
+            custom_payment_methods: item?.custom_payment_methods ?? [],
             club_url: item?.club_url ?? undefined,
             deregistration_in_progress: item?.deregistration_in_progress ?? false,
             onboarded,
