7b18730038d71dce301e2202825cf6a8e5f76362
gregtorrington <gregt@aleios.com>
2026-02-19 10:01:34 +0200

    feat: improving backend

--- functions/admin/club_member/get_all_club_members/get_all_club_members.ts
+++ functions/admin/club_member/get_all_club_members/get_all_club_members.ts
@@ -251,9 +251,9 @@ const getRegistrationPageData = async (
                 }
             );
 
-            if (memberType === "registered" && registration.registration_id !== club_member?.current_reg_id) {
+            if (memberType === "registered" && (registration.registration_id !== club_member?.current_reg_id || registration?.deregistered || club_member?.registered !== true)) {
                 continue;
-            } else if (memberType === "pending" && registration.registration_id !== club_member?.current_reg_id) {
+            } else if (memberType === "pending" && (registration.registration_id !== club_member?.current_reg_id || registration?.deregistered || club_member?.resubmission_required !== true || club_member?.registered === true)) {
                 continue;
             } else if (memberType === "previous" && registration.deregistered === false) {
                 continue;
