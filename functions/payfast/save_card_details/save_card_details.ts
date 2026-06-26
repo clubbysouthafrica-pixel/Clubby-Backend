00b3a2e8740519d89dcaf5dc33de91fb672d051d
gregtorrington <gregt@aleios.com>
2026-06-26 08:11:30 +0200

    feat: improving backend

--- functions/payfast/save_card_details/save_card_details.ts
+++ functions/payfast/save_card_details/save_card_details.ts
@@ -31,7 +31,7 @@ export const handler = async (event: any) => {
 
             m_payment_id: query_string_params.club_account_id,
 
-            amount: "0.00",
+            amount: "1.00",
             item_name: "Card Registration",
 
             payment_method: "cc",
