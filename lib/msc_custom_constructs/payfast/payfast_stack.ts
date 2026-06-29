2d9d9357d45f04bc4433f87c6e696652ca2daa5f
gregtorrington <gregt@aleios.com>
2026-06-29 10:10:04 +0200

    feat: increasing lambda timeout

--- lib/msc_custom_constructs/payfast/payfast_stack.ts
+++ lib/msc_custom_constructs/payfast/payfast_stack.ts
@@ -115,6 +115,7 @@ export class MSC_PayFastNestedStack extends Stack {
           "ses:SendEmail",
         ],
       },
+      timeout: 300,
       layers: [all_layers.jwt_layer, all_layers.axios_layer],
     });
 
